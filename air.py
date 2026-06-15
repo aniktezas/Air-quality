d8fa2863dc572061a14d7fdae3cf9cb3bd244166
Anik Tejas <aniktejas@gmail.com>
2026-06-16 04:56:01 +0530

    air quality update

--- air.py
+++ air.py
@@ -1,3 +1,5 @@
+import requests
+
 def interpret(pm25):
     if pm25 <= 50:
         return "Good"
@@ -5,6 +7,13 @@ def interpret(pm25):
         return "Moderate"
     else:
         return "Unhealthy"
-print(interpret(30))  # Output: Good
-print(interpret(75))  # Output: Moderate
-print(interpret(150)) # Output: Unhealthy       
+
+
+url = "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=12.92&longitude=79.13&current=pm2_5"
+
+response = requests.get(url)
+data = response.json()
+
+pm25 = data['current']['pm2_5']
+print("Vellore PM2.5 right now:", pm25)
+print("Air quality:", interpret(pm25))
